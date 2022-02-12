5520800acd9068142f3f57a5f3f5f9b9b16b3609
Duc N. PHAM <duc.pham@asnet.com.vn>
2022-02-12 15:52:51 +0700

    feat(Inventory): add update action

--- src/components/Inventory.tsx
+++ src/components/Inventory.tsx
@@ -7,6 +7,7 @@ export type StoreItem = {
   name: string;
   category: string;
   price: number;
+  enabled: boolean;
 };
 
 const inventoryItems: StoreItem[] = [
@@ -14,31 +15,36 @@ const inventoryItems: StoreItem[] = [
     id: 1,
     name: 'Apple iMac 27"',
     category: 'Computer',
-    price: 1000
+    price: 1000,
+    enabled: true,
   },
   {
     id: 2,
     name: 'Apple iPhone 13 Pro Max',
     category: 'Phone',
-    price: 1500
+    price: 1500,
+    enabled: true,
   },
   {
     id: 3,
     name: 'Apple MacBook Pro 13"',
     category: 'Laptop',
-    price: 1999
+    price: 1999,
+    enabled: true,
   },
   {
     id: 4,
     name: 'Apple MacBook Air 13"',
     category: 'Laptop',
-    price: 1500
+    price: 1500,
+    enabled: true,
   },
   {
     id: 5,
     name: 'Apple iPod Touch',
     category: 'Accessories',
-    price: 699
+    price: 699,
+    enabled: true,
   }
 ];
 
@@ -52,7 +58,8 @@ export default function Inventory() {
         id: 6,
         name: 'Apple Watch Series 5',
         category: 'Accessories',
-        price: 299
+        price: 299,
+        enabled: false,
       }
     ]);
   };
@@ -61,8 +68,24 @@ export default function Inventory() {
     setItems(prevItems => prevItems.filter(item => item.id !== id));
   }, []);
 
+  const updateItem = useCallback((id: number, newItem: StoreItem) => {
+    setItems(prevItems => {
+      const matchedItemIndex = prevItems.findIndex(item => item.id === id);
+
+      if (matchedItemIndex === -1) {
+        return prevItems;
+      };
+
+      return [
+        ...prevItems.slice(0, matchedItemIndex),
+        newItem,
+        ...prevItems.slice(matchedItemIndex + 1)
+      ];
+    });
+  }, []);
+
   return (
-    <div className="max-w-2xl mx-auto">
+    <div className="max-w-4xl mx-auto">
       <div className="flex flex-col">
         <div className="overflow-x-auto shadow-md sm:rounded-lg">
           <button className="mx-2 my-2 bg-blue-700 transition duration-150 ease-in-out hover:bg-blue-600 rounded text-white px-6 py-2 text-xs" onClick={addItem}>Add New Item</button>
@@ -82,17 +105,24 @@ export default function Inventory() {
                   <th scope="col" className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
                     Category
                   </th>
-                  <th scope="col" className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
+                  <th scope="col" className="py-3 px-6 text-xs font-medium tracking-wider text-right text-gray-700 uppercase dark:text-gray-400">
                     Price
                   </th>
+                  <th scope="col" className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
+                    Enabled
+                  </th>
                   <th scope="col" className="p-4">
-                    <span className="sr-only">Edit</span>
                   </th>
                 </tr>
               </thead>
               <tbody className="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
                 {items.map(item => (
-                  <InventoryItem key={item.id} item={item} onRemove={removeItem} />
+                  <InventoryItem
+                    key={item.id}
+                    item={item}
+                    onUpdate={updateItem}
+                    onRemove={removeItem}
+                  />
                 ))}
               </tbody>
             </table>
