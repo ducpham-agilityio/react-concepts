dd53d70cacc1734ddffd928c3d10f519d46005be
Duc N. PHAM <duc.pham@asnet.com.vn>
2022-02-12 15:52:24 +0700

    feat(InventoryItem): define enabled property

--- src/components/InventoryItem.tsx
+++ src/components/InventoryItem.tsx
@@ -1,14 +1,21 @@
-import { memo, useCallback } from 'react';
+import { memo } from 'react';
+
+import Toggle from './Toggle';
 import { StoreItem } from './Inventory';
 
 interface InventoryItemProps {
   item: StoreItem;
   onRemove: (id: number) => void;
+  onUpdate: (id: number, item: StoreItem) => void;
 }
 
-function InventoryItem({ item, onRemove }: InventoryItemProps) {
+function InventoryItem({ item, onRemove, onUpdate }: InventoryItemProps) {
   const handleRemove = () => {
     onRemove(item.id);
+  };
+
+  const handleFieldChange = (field: keyof StoreItem, value: StoreItem[typeof field]) => {
+    onUpdate(item.id, { ...item, [field]: value });
   };
 
   return (
@@ -19,9 +26,17 @@ function InventoryItem({ item, onRemove }: InventoryItemProps) {
           <label htmlFor="checkbox-table-1" className="sr-only">checkbox</label>
         </div>
       </td>
-      <td className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">{item.name}</td>
-      <td className="py-4 px-6 text-sm font-medium text-gray-500 whitespace-nowrap dark:text-white">{item.category}</td>
-      <td className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">${item.price}</td>
+      <td className="py-4 px-6 text-sm text-left font-medium text-gray-900 whitespace-nowrap dark:text-white">{item.name}</td>
+      <td className="py-4 px-6 text-sm text-left font-medium text-gray-500 whitespace-nowrap dark:text-white">{item.category}</td>
+      <td className="py-4 px-6 text-sm text-right font-medium text-gray-900 whitespace-nowrap dark:text-white">${item.price}</td>
+      <td className="py-4 px-6 text-sm text-left font-medium text-gray-900 whitespace-nowrap dark:text-white">
+        <Toggle
+          value={item.enabled}
+          onChange={value => {
+            handleFieldChange('enabled', value);
+          }}
+        />
+      </td>
       <td className="py-4 px-6 text-sm font-medium text-right whitespace-nowrap">
         <a href="#" className="text-blue-600 dark:text-blue-500 hover:underline" onClick={handleRemove}>Remove</a>
       </td>
