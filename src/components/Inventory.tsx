414caea8c4daf56372f0b24b1a8c45bd45429001
Duc N. PHAM <duc.pham@asnet.com.vn>
2022-02-13 21:07:47 +0700

    feat(inventory): replace toggles with checkboxes

--- src/components/Inventory.tsx
+++ src/components/Inventory.tsx
@@ -93,12 +93,6 @@ export default function Inventory() {
             <table className="min-w-full divide-y divide-gray-200 table-fixed dark:divide-gray-700">
               <thead className="bg-gray-100 dark:bg-gray-700">
                 <tr>
-                  <th scope="col" className="p-4">
-                    <div className="flex items-center">
-                      <input id="checkbox-all" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                      <label htmlFor="checkbox-all" className="sr-only">checkbox</label>
-                    </div>
-                  </th>
                   <th scope="col" className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
                     Product Name
                   </th>
--- src/components/InventoryItem.tsx
+++ src/components/InventoryItem.tsx
@@ -20,20 +20,16 @@ function InventoryItem({ item, onRemove, onUpdate }: InventoryItemProps) {
 
   return (
     <tr className="hover:bg-gray-100 dark:hover:bg-gray-700">
-      <td className="p-4 w-4">
-        <div className="flex items-center">
-          <input id="checkbox-table-1" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-          <label htmlFor="checkbox-table-1" className="sr-only">checkbox</label>
-        </div>
-      </td>
       <td className="py-4 px-6 text-sm text-left font-medium text-gray-900 whitespace-nowrap dark:text-white">{item.name}</td>
       <td className="py-4 px-6 text-sm text-left font-medium text-gray-500 whitespace-nowrap dark:text-white">{item.category}</td>
       <td className="py-4 px-6 text-sm text-right font-medium text-gray-900 whitespace-nowrap dark:text-white">${item.price}</td>
       <td className="py-4 px-6 text-sm text-left font-medium text-gray-900 whitespace-nowrap dark:text-white">
-        <Toggle
-          value={item.enabled}
-          onChange={value => {
-            handleFieldChange('enabled', value);
+        <input
+          type="checkbox"
+          className="rounded text-blue-500"
+          checked={item.enabled}
+          onChange={() => {
+            handleFieldChange('enabled', !item.enabled);
           }}
         />
       </td>
