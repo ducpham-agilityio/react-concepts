53ec4f853edfa0805b5f54bf944366fb2774ddf0
Duc N. PHAM <duc.pham@asnet.com.vn>
2022-03-05 22:32:56 +0700

    feat(collection): define custom hooks

---
+++ src/hooks/useCollection.ts
@@ -0,0 +1,39 @@
+import { useCallback, useState } from 'react';
+
+interface HasId {
+  id: number;
+}
+
+export default function useCollection<T extends HasId>(initialCollection: T[]) {
+  const [collection, setCollection] = useState(initialCollection);
+
+  const addItem = useCallback((item: T) => {
+    setCollection(prevCollection => [...prevCollection, item]);
+  }, []);
+
+  const removeItem = useCallback((id: number) => {
+    setCollection(prevCollection => prevCollection.filter(item => id !== item.id));
+  }, []);
+
+  const updateItem = useCallback((item: T) => {
+    setCollection(prevCollection => {
+      const matchedIndex = prevCollection.findIndex(({ id }) => id === item.id);
+
+      if (matchedIndex === -1) {
+        return prevCollection;
+      }
+      return [
+        ...prevCollection.slice(0, matchedIndex),
+        item,
+        ...prevCollection.slice(matchedIndex + 1),
+      ];
+    });
+  }, []);
+
+  return {
+    collection,
+    addItem,
+    removeItem,
+    updateItem,
+  };
+}
