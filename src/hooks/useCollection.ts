dfdda43d218cd17ddd00c437ba016107341a66c5
Duc N. PHAM <duc.pham@asnet.com.vn>
2022-09-27 15:35:06 +0700

    Refactor hook to use store

--- src/hooks/useCollection.ts
+++ src/hooks/useCollection.ts
@@ -1,80 +1,9 @@
 import { useCallback, useReducer } from 'react';
 
-export interface HasId {
-  id: number;
-}
+import { HasId } from '../types/base';
 
-
-interface CollectionState<T extends HasId> {
-  collection: T[];
-}
-
-enum ACTION_TYPE {
-  CREATE = 'CREATE',
-  DELETE = 'DELETE',
-  UPDATE = 'UPDATE',
-};
-
-type CreateItem<T extends HasId> = {
-  type: ACTION_TYPE.CREATE,
-  payload: T;
-}
-
-type UpdateItem<T extends HasId> = {
-  type: ACTION_TYPE.UPDATE,
-  payload: T;
-}
-
-type DeleteItem = {
-  type: ACTION_TYPE.DELETE,
-  payload: number;
-}
-
-type CollectionActions<T extends HasId> =
-  | CreateItem<T>
-  | UpdateItem<T>
-  | DeleteItem;
-
-const reducerFactory =  <T extends HasId>() => (state: CollectionState<T>, action: CollectionActions<T>): CollectionState<T> => {
-  switch(action.type) {
-    case ACTION_TYPE.CREATE: {
-      return {
-        ...state,
-        collection: [
-          ...state.collection,
-          action.payload
-        ],
-      };
-    }
-
-    case ACTION_TYPE.UPDATE: {
-      const matchedIndex = state.collection.findIndex(({ id }) => id === action.payload.id);
-
-      if (matchedIndex === -1) {
-        return state;
-      }
-
-      return {
-        ...state,
-        collection: [
-          ...state.collection.slice(0, matchedIndex),
-          action.payload,
-          ...state.collection.slice(matchedIndex + 1),
-        ]
-      };
-    }
-
-    case ACTION_TYPE.DELETE: {
-      return {
-        ...state,
-        collection: state.collection.filter(({ id }) => id !== action.payload)
-      }
-    }
-
-    default:
-      return state;
-  }
-}
+import { reducerFactory } from '../store/collection/reducer';
+import { ACTION_TYPE } from '../store/collection/types';
 
 export default function useCollection<T extends HasId>(initialCollection: T[]){
   const [state, dispatch] = useReducer(reducerFactory<T>(), { collection: initialCollection });
