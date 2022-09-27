5745f4a007368ecb3d2e83b3424413e4ed501984
Duc N. PHAM <duc.pham@asnet.com.vn>
2022-09-27 12:26:19 +0700

    Apply useReducer to hooks

--- src/hooks/useCollection.ts
+++ src/hooks/useCollection.ts
@@ -1,39 +1,109 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useReducer } from 'react';
 
-interface HasId {
+export interface HasId {
   id: number;
 }
 
-export default function useCollection<T extends HasId>(initialCollection: T[] | (() => T[])){
-  const [collection, setCollection] = useState(initialCollection);
 
-  const addItem = useCallback((item: T) => {
-    setCollection(prevCollection => [...prevCollection, item]);
+interface CollectionState<T extends HasId> {
+  collection: T[];
+}
+
+enum ACTION_TYPE {
+  CREATE = 'CREATE',
+  DELETE = 'DELETE',
+  UPDATE = 'UPDATE',
+};
+
+type CreateItem<T extends HasId> = {
+  type: ACTION_TYPE.CREATE,
+  payload: T;
+}
+
+type UpdateItem<T extends HasId> = {
+  type: ACTION_TYPE.UPDATE,
+  payload: T;
+}
+
+type DeleteItem = {
+  type: ACTION_TYPE.DELETE,
+  payload: number;
+}
+
+type CollectionActions<T extends HasId> =
+  | CreateItem<T>
+  | UpdateItem<T>
+  | DeleteItem;
+
+const reducerFactory =  <T extends HasId>() => (state: CollectionState<T>, action: CollectionActions<T>): CollectionState<T> => {
+  switch(action.type) {
+    case ACTION_TYPE.CREATE: {
+      return {
+        ...state,
+        collection: [
+          ...state.collection,
+          action.payload
+        ],
+      };
+    }
+
+    case ACTION_TYPE.UPDATE: {
+      const matchedIndex = state.collection.findIndex(({ id }) => id === action.payload.id);
+
+      if (matchedIndex === -1) {
+        return state;
+      }
+
+      return {
+        ...state,
+        collection: [
+          ...state.collection.slice(0, matchedIndex),
+          action.payload,
+          ...state.collection.slice(matchedIndex + 1),
+        ]
+      };
+    }
+
+    case ACTION_TYPE.DELETE: {
+      return {
+        ...state,
+        collection: state.collection.filter(({ id }) => id !== action.payload)
+      }
+    }
+
+    default:
+      return state;
+  }
+}
+
+export default function useCollection<T extends HasId>(initialCollection: T[]){
+  const [state, dispatch] = useReducer(reducerFactory<T>(), { collection: initialCollection });
+
+  const createItem = useCallback((item: T) => {
+    dispatch({
+      type: ACTION_TYPE.CREATE,
+      payload: item,
+    });
   }, []);
 
-  const removeItem = useCallback((id: number) => {
-    setCollection(prevCollection => prevCollection.filter(item => id !== item.id));
+  const deleteItem = useCallback((id: number) => {
+    dispatch({
+      type: ACTION_TYPE.DELETE,
+      payload: id,
+    });
   }, []);
 
   const updateItem = useCallback((item: T) => {
-    setCollection(prevCollection => {
-      const matchedIndex = prevCollection.findIndex(({ id }) => id === item.id);
-
-      if (matchedIndex === -1) {
-        return prevCollection;
-      }
-      return [
-        ...prevCollection.slice(0, matchedIndex),
-        item,
-        ...prevCollection.slice(matchedIndex + 1),
-      ];
+    dispatch({
+      type: ACTION_TYPE.UPDATE,
+      payload: item,
     });
   }, []);
 
   return {
-    collection,
-    addItem,
-    removeItem,
+    state,
+    createItem,
+    deleteItem,
     updateItem,
   };
 }
